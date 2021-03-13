ed789190c3ef048755440de58e23592735f706af
John Alvarez <jcalvare@Diandre-iPhone.localdomain>
2021-03-13 14:25:29 -0500

    initialze project

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+
+app.set('view engine', 'ejs');
+
+app.use('/', function (req, res, next) {
+	console.log('Request Url:' + req.url);
+	next();
+});
+
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+
+
+
+
+app.listen(port);
